f71f8d55cfd83d3cf51fd910e1e330d0700844f8
jacobmc352 <jacobmc352@gmail.com>
2024-03-01 19:37:14 -0600

    changes

--- test/jacob.test.js
+++ test/jacob.test.js
@@ -14,12 +14,14 @@ it("Should get a login token",async ()=>{
         })
     }
 
-    const response = await fetch('https://dev.stedi.me/login',options);
+    const response = await fetch('http://localhost:3000/jacob')
 
     token = await response.text();
     console.log("Token "+token);
+    const name = await response.text();
     const status = response.status;
-
+    
+    expect(name).toBe("jacob");
     expect(status).toBe(200);
     expect(token.length).toBe(36);
 
